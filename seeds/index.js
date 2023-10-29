4ce1603a783edbc367780598f30d8115d89b05d2
Lenzlee Wheeler <lwheeler@luc.edu>
2023-10-29 18:45:46 -0500

    updated seeds indexjs

--- seeds/index.js
+++ seeds/index.js
@@ -1,10 +1,12 @@
 const sequelize = require('../config/connection');
 const seedGame = require("./gameData");
+// const seedUsers = require('./userData');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
 
   await seedGame();
+  // await seedUsers();
 
   process.exit(0);
 };
