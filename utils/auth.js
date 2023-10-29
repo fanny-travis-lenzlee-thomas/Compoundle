992c1716610d6bedd62da960922f9e7eb9ddd730
Lenzlee Wheeler <lwheeler@luc.edu>
2023-10-29 15:44:19 -0500

    updated file structure

--- utils/auth.js
+++ utils/auth.js
@@ -1,7 +1,7 @@
 const withAuth = (req, res, next) => {
   // If the user is not logged in, redirect the user to the login page
   if (!req.session.loggedIn) {
-    res.redirect('/login');
+    res.redirect("/login");
   } else {
     // If the user is logged in, execute the route function that will allow them to view the words challenge
     // We call next() if the user is authenticated
