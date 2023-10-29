385d651e9aba2ebc4e6b42909cc1374359f4a176
Lenzlee Wheeler <lwheeler@luc.edu>
2023-10-29 15:49:42 -0500

    moved css and js files to assets dir

--- config/connection.js
+++ config/connection.js
@@ -6,8 +6,8 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: '127.0.0.1',
-    dialect: 'mysql',
+    host: "127.0.0.1",
+    dialect: "mysql",
     port: 3306,
   }
 );
